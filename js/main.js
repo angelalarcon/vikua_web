b48ddf488d5163602fb269be61407245b403ee25
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2015-03-15 18:32:26 -0430

    city gif and content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	$('section').css({'height':($(window).height())+'px'})
 
+	if ($('.menu .nav-pills li').hasClass('active')) {
+		$('.building').addClass('glow');
+	} else {
+		$('.building').removeClass('glow');
+	};
 	
+	$('.menu .nav-pills li').click(function(){
+		if ($(this).hasClass('active')) {
+			$('.building').addClass('glow');
+		} else {
+			$('.building').removeClass('glow');
+		};
+	})
 });
